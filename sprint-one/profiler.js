81060dc436e347ba1a3ea173c93d034088378a1c
dsernst <github@dsernst.com>
2014-12-10 22:18:43 -0800

    add: profiling tests and results

---
+++ sprint-one/profiler.js
@@ -0,0 +1,28 @@
+var stacks = [];
+var queues = [];
+var tests = 1000000;
+
+console.log(variant + " * " + tests);
+
+console.time("stack construction");
+for (var example = 0; example < tests; example++){
+    if(variant === 'pseudoclassical'){
+      stacks[example] = new Stack();
+    } else {
+      stacks[example] = Stack();
+    }
+}
+console.timeEnd("stack construction");
+
+
+console.time("queue construction");
+for (var example = 0; example < tests; example++){
+    if(variant === 'pseudoclassical'){
+      queues[example] = new Queue();
+    } else {
+      queues[example] = Queue();
+    }
+}
+console.timeEnd("queue construction");
+
+console.log("");
