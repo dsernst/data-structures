ec2470b19c305ebcfaa9018295512aa2dd2a0781
dsernst <github@dsernst.com>
2014-12-11 14:41:20 -0800

    Implements tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,23 +3,42 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+// Constant time
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+// Linear time
+  var found = false;
 
+  var searchTree = function(node) {
+    if (node.value === target) {
+      found = true;
+      return;
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      searchTree(node.children[i]);
+    }
+  };
+
+  searchTree(this);
+
+  return found;
 };
 
 
