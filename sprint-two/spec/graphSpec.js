ea4345cf3dfed355f28da1725c1b2a27fe6c234d
dsernst <github@dsernst.com>
2014-12-11 15:01:59 -0800

    Implements graph

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -41,6 +41,8 @@ describe('graph', function() {
     graph.addNode('satsumas');
     graph.addEdge('satsumas', 'apples');
     expect(graph.hasEdge('apples', 'satsumas')).to.equal(true);
+    graph.removeEdge('satsumas', 'apples');
+    expect(graph.hasEdge('apples', 'satsumas')).to.equal(false);
   });
 
   it('should execute a callback on each node in the graph', function() {
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,47 @@
 
 
 var Graph = function(){
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(node){
+// Constant time
+  this.nodes[node] = {};
 };
 
 Graph.prototype.contains = function(node){
+// O(1)
+  return this.nodes.hasOwnProperty(node);
+}
 };
 
 Graph.prototype.removeNode = function(node){
+// O(1)
+  delete this.nodes[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+// O(1)
+  return this.nodes[fromNode].hasOwnProperty(toNode);
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+// O(1)
+  this.nodes[fromNode][toNode] = toNode;
+  this.nodes[toNode][fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+// O(1)
+  delete this.nodes[fromNode][toNode];
+  delete this.nodes[toNode][fromNode];
 };
 
 Graph.prototype.forEachNode = function(cb){
+// O(n)
+  for (var node in this.nodes) {
+    cb(node, this.nodes);
+  }
 };
 
 /*
