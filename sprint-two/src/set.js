a0cbc956e27be204e903da19950fd2dd3bcca8de
dsernst <github@dsernst.com>
2014-12-11 15:11:55 -0800

    Implements set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+// O(1)
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+// O(1)
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item){
+// O(1)
+  delete this._storage[item];
 };
 
 /*
