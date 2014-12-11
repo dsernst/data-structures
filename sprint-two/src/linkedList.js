886a621cd7041c07d3dc935278cacb7e5390da63
dsernst <github@dsernst.com>
2014-12-11 14:27:18 -0800

    Implements linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+  // Constant time O(3)
+    var node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function(){
+  // Constant time O(3)
+    var oldHead = list.head;
+    list.head = oldHead.next;
+    // Maybe delete the old head node object?
+    return oldHead.value;
   };
 
   list.contains = function(target){
+  // Linear time O(n)
+    var found = false;
+    var position = list.head;
+    while (!found && position !== null) {
+      if (position.value === target) {
+        found = true;
+      }
+      position = position.next;
+    }
+    return found;
   };
 
   return list;
