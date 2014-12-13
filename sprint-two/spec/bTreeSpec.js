344e90fdef95b282adbbb463f9c1d82f789ff91c
dsernst <github@dsernst.com>
2014-12-13 11:43:10 -0800

    progress BTree

--- sprint-two/spec/bTreeSpec.js
+++ sprint-two/spec/bTreeSpec.js
@@ -5,12 +5,26 @@ describe('bTree', function() {
     b = new BTree(3);
   });
 
-  it('should log stuff, allow inserts, and sort root node', function() {
+  it('should log stuff, allow inserts, sort nodes, handle splits, and pass to children', function() {
     b.insert(50);
     expect(b.values[0]).to.equal(50);
     b.insert(40);
     expect(b.values[0]).to.equal(40);
+    b.insert(60);
+    expect(b.children.length).to.equal(2);
+    expect(b.children[0].values[0]).to.equal(40);
+    expect(b.children[1].values[0]).to.equal(60);
     console.log(b);
+    b.insert(30);
+    expect(b.children[0].values[0]).to.equal(30);
+    b.insert(70);
+    expect(b.children[1].values[1]).to.equal(70);
+    b.insert(55);
+    expect(b.children[1].values[0]).to.equal(55);
+    expect(b.children[2].values[0]).to.equal(70);
+    expect(b.values[1]).to.equal(60);
+    // b.insert(35);
+    debugger;
   });
 
 });
--- sprint-two/src/bTree.js
+++ sprint-two/src/bTree.js
@@ -5,24 +5,66 @@ var BTree = function (order) {
 };
 
 BTree.prototype.insert = function (value) {
-  if ( this.canInsert() ) {
+  var emptySlots = this.children.length - this.values.length;
+  var destination = this.pickChild(value);
+  if (emptySlots > 1) {
+    destination = null;
+  }
+  if ( destination !== null ) {
+    this.insert.call(this.children[destination], value);
+  } else {
     this.values.push(value);
     this.sortNode();
+    if (this.isOverloaded()) {
+      this.split();
+    }
   }
-  // else
-    // find child to push to
-    // insert.call(this.child[found], value);
 
-};
-
-BTree.prototype.isLeaf = function () {
-  return this.children.length === 0;
 };
 
 BTree.prototype.sortNode = function() {
   this.values.sort(function(a,b) {return a - b;})
 };
 
-BTree.prototype.canInsert = function () {
-  return this.values.length < this.order;
+BTree.prototype.isOverloaded = function () {
+  return this.values.length === this.order;
+};
+
+BTree.prototype.split = function () {
+  var leftSplit = new BTree(this.order);
+  var rightSplit = new BTree(this.order);
+
+  leftSplit.values = this.values.splice(0,1);
+  var middle = this.values.splice(0,1)[0];
+  rightSplit.values = this.values.splice(0,1);
+  if (this.parent) {
+    var parent = this.parent;
+    var destination = parent.pickChild(leftSplit.values[0]);
+    parent.children.splice(destination,1);
+    parent.children.splice(destination, 0, leftSplit, rightSplit);
+    parent.insert(middle);
+  } else {
+    this.values[0] = middle;
+    this.children = [leftSplit, rightSplit]
+  // leftSplit.parent = this;
+  // rightSplit.parent = this;
+  // this.children.push(leftSplit, rightSplit);
+  }
+};
+
+BTree.prototype.pickChild = function (value) {
+  if (this.children.length !== 0) {
+    for (var destination = 0; destination < this.values.length; destination++) {
+      if (value < this.values[destination]) {
+        break;
+      }
+    }
+    return destination;
+  }
+  return null;
+};
+
+
+BTree.prototype.isLeaf = function () {
+  return this.children.length === 0;
 };
